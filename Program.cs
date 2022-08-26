9be55e47ab59eaaa0f49aca763acea3c49f05e34
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-08-26 22:32:02 +0700

    wip

--- Program.cs
+++ Program.cs
@@ -5,14 +5,55 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-app.Map("/map1", HandleMapTest1);
+/* Basic */
+// app.Map("/map1", HandleMapTest1);
+// app.Map("/map2", HandleMapTest2);
 
-app.Map("/map2", HandleMapTest2);
+/* Nested map */
+// app.Map("/level", levelApp => {
+//     levelApp.Map("/map1", HandleMapTest1);
+//
+//     levelApp.Map("/map2", HandleMapTest2);
+// });
+
+/* Multiple segments */
+// app.Map("/map1/seg1", HandleMapTest1);
+
+/* MapWhen */
+// app.MapWhen(context => context.Request.Query.ContainsKey("branch"), HandleBranch);
+
+/* UseWhen */
+// app.UseWhen(context => context.Request.Query.ContainsKey("branch"),
+//     appBuilder => HandleBranchAndRejoin(appBuilder));
 
 app.Run(async context =>
 {
     await context.Response.WriteAsync("Hello from non-Map delegate.");
 });
+
+void HandleBranchAndRejoin(IApplicationBuilder app)
+{
+    var logger = app.ApplicationServices.GetRequiredService<ILogger<Program>>(); 
+
+    app.Use(async (context, next) =>
+    {
+        var branchVer = context.Request.Query["branch"];
+        logger.LogInformation("Branch used = {branchVer}", branchVer);
+
+        // Do work that doesn't write to the Response.
+        await next();
+        // Do other work that doesn't write to the Response.
+    });
+}
+
+static void HandleBranch(IApplicationBuilder app) 
+{
+    app.Run(async context =>
+    {
+        var branchVer = context.Request.Query["branch"];
+        await context.Response.WriteAsync($"Branch used = {branchVer}");
+    });
+}
 
 static void HandleMapTest1(IApplicationBuilder app)
 {
